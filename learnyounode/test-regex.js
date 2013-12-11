599c8701823a301209bb3ad83a8c22407f553e66
Maggie Nelson <maggie@maggienelson.com>
2013-12-10 16:51:57 -0800

    don't need these files

--- learnyounode/test-regex.js
+++
@@ -1,4 +0,0 @@
-var str = "hello world";
-var patt = /hello/g;
-var result = patt.test(str);
-console.log("result: " + result);
--- learnyounode/test-regex2.js
+++
@@ -1,5 +0,0 @@
-var str = "omg.txt";
-//var patt = new RegExp(/hello/g);
-var patt = new RegExp(/\.txt$/g);
-var result = patt.test(str);
-console.log("result: " + result);
