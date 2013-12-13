d34ec6e12019047d1d25742160f048939f777758
Maggie Nelson <maggie@maggienelson.com>
2013-12-13 14:53:23 -0800

    fixed parameter parsing order

--- learnyounode/http-file-server.js
+++ learnyounode/http-file-server.js
@@ -1,12 +1,11 @@
 var http = require('http');
 var fs = require('fs');
 
-var port = Number(process.argv[2]); // make sure it's numeric
-var file = process.argv[3];
+var file = process.argv[2];
 
 var server = http.createServer( function (request, response) {
   // stream file as response
   fs.createReadStream(file).pipe(response);
 });
 
-server.listen(port);
+server.listen(8000);
